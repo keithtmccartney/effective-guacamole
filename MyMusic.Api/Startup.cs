7793ce45ed812dc34574a3b9450c6b99baf48241
Keith <keithtmccartney@hotmail.com>
2020-10-02 14:28:22 +0100

    Adding Swagger (Swashbuckle.AspNetCore) version 5.0.0-rc3 ("dotnet add MyMusic.Api/MyMusic.Api.csproj package Swashbuckle.AspNetCore --version 5.0.0-rc3") to the Api project and applying its inclusion into the startup logic. Testing it out at localhost:5001 ("dotnet run -p MyMusic.Api/MyMusic.Api.csproj"), looks good.

--- MyMusic.Api/Startup.cs
+++ MyMusic.Api/Startup.cs
@@ -11,9 +11,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using MyMusic.Core;
 using MyMusic.Data;
 using MyMusic.Services.Services;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MyMusic.Api
 {
@@ -34,6 +36,12 @@ namespace MyMusic.Api
             services.AddTransient<IArtistService, ArtistService>(); // Add dependency injection for our Artist Service.
             services.AddTransient<IMusicService, MusicService>(); // Add dependency injection for our Music Service.
             services.AddDbContext<MyMusicDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default"), x => x.MigrationsAssembly("MyMusic.Data")));
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "My Music",
+                    Version = "v1"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +61,14 @@ namespace MyMusic.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Music V1");
             });
         }
     }
