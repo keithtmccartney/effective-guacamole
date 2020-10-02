2304b576f15bc3da5e86d5b5a8d42584f42cf120
Keith <keithtmccartney@hotmail.com>
2020-10-02 07:51:58 +0100

    Adding new Service Interfaces for the Artist and Music logic.

---
+++ MyMusic.Services/Services/IMusicService.cs
@@ -0,0 +1,18 @@
+﻿using MyMusic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic.Services.Services
+{
+    public interface IMusicService
+    {
+        Task<IEnumerable<Music>> GetAllWithArtist();
+        Task<Music> GetMusicById(int id);
+        Task<IEnumerable<Music>> GetMusicByArtistId(int artistId);
+        Task<Music> CreateMusic(Music newMusic);
+        Task UpdateMusic(Music musicToBeUpdated, Music music);
+        Task DeleteMusic(Music music);
+    }
+}
