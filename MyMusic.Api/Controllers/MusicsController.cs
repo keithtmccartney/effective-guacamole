5ceb4a926e1357e8d9ac5df027ca1356be8042c4
Keith <keithtmccartney@hotmail.com>
2020-10-02 14:52:14 +0100

    Removing stock Weather Controller and creating Musics Controller, testing on Swagger ("dotnet run -p MyMusic.Api/MyMusic.Api.csproj") provides a Successful 200 which is always nice to see.

---
+++ MyMusic.Api/Controllers/MusicsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyMusic.Core.Models;
+using MyMusic.Services.Services;
+
+namespace MyMusic.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicsController : ControllerBase
+    {
+        private readonly IMusicService _musicService;
+
+        public MusicsController(IMusicService musicService, IMapper mapper)
+        {
+            this._musicService = musicService;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<IEnumerable<Music>>> GetAllMusics()
+        {
+            var musics = await _musicService.GetAllWithArtist();
+
+            return Ok(musics);
+        }
+    }
+}
