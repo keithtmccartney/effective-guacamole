3acf425c110f69bdda8d342d05a42b87538530b3
Keith <keithtmccartney@hotmail.com>
2020-10-02 07:22:56 +0100

    The base of the code following the Repository and Unit of Work patterns has now been set up.

---
+++ MyMusic.Core/IUnitofWork.cs
@@ -0,0 +1,15 @@
+﻿using MyMusic.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMusic.Core
+{
+    public interface IUnitofWork : IDisposable
+    {
+        IMusicRepository Musics { get; }
+        IArtistRepository Artists { get; }
+        Task<int> CommitAsync();
+    }
+}
