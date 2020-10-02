8e0eb72f7b9a77109087cb006f0dc3eb68fd33b6
Keith <keithtmccartney@hotmail.com>
2020-10-02 07:16:15 +0100

    Fixing typo in folder-structure.

--- MyMusic.Core/Repositories/IRepository.cs
+++ MyMusic.Core/Repositories/IRepository.cs
@@ -4,7 +4,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyMusic.Core.Repository
+namespace MyMusic.Core.Repositories
 {
     public interface IRepository<TEntity> where TEntity : class
     {
