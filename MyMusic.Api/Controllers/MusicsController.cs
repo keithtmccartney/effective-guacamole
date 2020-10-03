2da6342695cd52f08f7343538308960bd953b3ad
Keith <keithtmccartney@hotmail.com>
2020-10-03 08:13:16 +0100

    Adding other HTTP verbs to the Musics Controller, adding Artist and Music Save Resources and applying their usage to the Musics Controller.

--- MyMusic.Api/Controllers/MusicsController.cs
+++ MyMusic.Api/Controllers/MusicsController.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyMusic.Api.Resources;
+using MyMusic.Api.Validators;
 using MyMusic.Core.Models;
 using MyMusic.Services.Services;
 
@@ -30,7 +31,7 @@ namespace MyMusic.Api.Controllers
             var musics = await _musicService.GetAllWithArtist();
             var musicResources = _mapper.Map<IEnumerable<Music>, IEnumerable<MusicResource>>(musics);
 
-            return Ok(musics);
+            return Ok(musicResources);
         }
 
         [HttpGet("{id}")]
@@ -40,6 +41,68 @@ namespace MyMusic.Api.Controllers
             var musicResource = _mapper.Map<Music, MusicResource>(music);
 
             return Ok(musicResource);
+        }
+
+        [HttpPost("")]
+        public async Task<ActionResult<MusicResource>> CreateMusic([FromBody] SaveMusicResource saveMusicResource) // "Notice the [FromBody] attribute before the parameter, it indicates that this object is coming from the requests body".
+        {
+            var validator = new SaveMusicResourceValidator();
+            var validationResult = await validator.ValidateAsync(saveMusicResource);
+
+            if (!validationResult.IsValid)
+                return BadRequest(validationResult.Errors); // this needs refining, but for demo it is ok. How could/should this be refined for production?
+
+            var musicToCreate = _mapper.Map<SaveMusicResource, Music>(saveMusicResource);
+
+            var newMusic = await _musicService.CreateMusic(musicToCreate);
+
+            var music = await _musicService.GetMusicById(newMusic.Id);
+
+            var musicResource = _mapper.Map<Music, MusicResource>(music);
+
+            return Ok(musicResource);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<MusicResource>> UpdateMusic(int id, [FromBody] SaveMusicResource saveMusicResource) // "Id is coming from the request url and saveMusicResource is coming from the request body".
+        {
+            var validator = new SaveMusicResourceValidator();
+            var validationResult = await validator.ValidateAsync(saveMusicResource);
+
+            var requestIsInvalid = id == 0 || !validationResult.IsValid;
+
+            if (requestIsInvalid)
+                return BadRequest(validationResult.Errors); // this needs refining, but for demo it is ok. How could/should this be refined for production?
+
+            var musicToBeUpdate = await _musicService.GetMusicById(id);
+
+            if (musicToBeUpdate == null)
+                return NotFound();
+
+            var music = _mapper.Map<SaveMusicResource, Music>(saveMusicResource);
+
+            await _musicService.UpdateMusic(musicToBeUpdate, music);
+
+            var updatedMusic = await _musicService.GetMusicById(id);
+            var updatedMusicResource = _mapper.Map<Music, MusicResource>(updatedMusic);
+
+            return Ok(updatedMusicResource);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMusic(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var music = await _musicService.GetMusicById(id);
+
+            if (music == null)
+                return NotFound();
+
+            await _musicService.DeleteMusic(music);
+
+            return NoContent();
         }
     }
 }
